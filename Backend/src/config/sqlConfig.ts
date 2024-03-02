a147d4c8de521aebbcf2fb866e0e07891f1e47af
KiamaM <muriithikiamad1@gmail.com>
2024-03-02 16:16:04 +0300

    organized folder structure

--- Backend/src/config/sqlConfig.ts
+++ Backend/src/config/sqlConfig.ts
@@ -1,24 +1,24 @@
-import * as dotenv from 'dotenv'
+import mssql from 'mssql'
+import dotenv from 'dotenv'
+
 dotenv.config()
 
+console.log(process.env?.['DB_NAME']);
+
 export const sqlConfig = {
-    user: 'sa',
-    password: '09909090Mkk',
-    database: 'SHOPSIS',
-    server:'DESKTOP-B56002J\\KIMWETICHKKORIR',
-  
-    pool: {
-      max: 10,
-      min: 0, 
-      idleTimeoutMillis: 30000,
-      
-    },
-    options: {
-      encrypt: false, 
-      trustServerCertificate: true
-    }
+  user: process.env?.['DB_USER'] as string,
+  password: process.env?.['DB_PWD'] as string,
+  database: process.env?.['DB_NAME'] as string,
+  server: process.env?.['SERVER'] as string,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
   }
+}
 
-  console.log(sqlConfig);
-
-  
+console.log(sqlConfig);
