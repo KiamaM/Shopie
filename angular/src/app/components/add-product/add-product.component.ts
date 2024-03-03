74fba89117f72fd9f4bd530ed36363f26b17f403
Your Name <janengene12@gmail.com>
2024-03-03 16:29:43 +0300

    Added add-product form

--- angular/src/app/components/add-product/add-product.component.ts
+++ angular/src/app/components/add-product/add-product.component.ts
@@ -6,51 +6,20 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [ReactiveFormsModule,FormsModule,CommonModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
-productForm!:FormGroup
-successMsg:string =''
-showSuccessMessage:boolean =false
+  productForm!: FormGroup
+  successMsg: string = ''
+  showSuccessMessage: boolean = false
 
-constructor(private router:Router, private fb:FormBuilder){
-  // this.productForm = this.fb.group({
-  //   tourType: ['', [Validators.required]],
-  //   image: ['', [Validators.required]],
-  //   destination: ['', [Validators.required]],
-  //   description: ['', [Validators.required]],
-  //   price: ['', [Validators.required]],
-  //   startDate: ['', [Validators.required]],
-  //   endDate: ['', [Validators.required]]
-  // })
-}
+  constructor(private router: Router, private fb: FormBuilder) {
 
-onSubmit(){
-  // if(this.productForm.valid){
-  //   this.tourservice.createTour(
-  //     this.tourForm.value.tourType,
-  //     this.tourForm.value.image,
-  //     this.tourForm.value.destination,
-  //     this.tourForm.value.description,
-  //     this.tourForm.value.price,
-  //     this.tourForm.value.startDate,
-  //     this.tourForm.value.endDate)
-  //     .subscribe((response) => {
-  //       console.log(response);
-  //     });
+  }
 
-  //   this.successMsg = 'Tour successfully created';
-  //   this.showSuccessMessage = true;
+  onSubmit() {
 
-  //   this.tourForm.reset();
-
-  //   setTimeout(() => {
-  //     this.showSuccessMessage = false
-  //   }, 2000);
-  // } else {
-  //   this.tourForm.markAllAsTouched();
-  // }
-}
+  }
 }
