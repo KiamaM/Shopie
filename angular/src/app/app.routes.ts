c4da0eedeccb74ae874c42c336f5946280ee179f
Your Name <janengene12@gmail.com>
2024-03-03 15:52:37 +0300

    Added add-product form

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ResetComponent } from './components/reset/reset.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +15,8 @@ export const routes: Routes = [
     {path:'navbar',component:NavbarComponent},
     {path:'register',component:RegisterComponent},
     { path: 'login', component: LoginComponent },
-    {path:'reset', component:ResetComponent}
-];
+    {path:'reset', component:ResetComponent},
+    {path:'admin',component:AdminPageComponent, 
+     children: [
+        { path: 'add-product', component: AddProductComponent }]}
+]
